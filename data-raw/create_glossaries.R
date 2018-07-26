45a4597a0859fdfeb34d22e4d6e89771db1af685
Alex Hayes <alexpghayes@gmail.com>
2018-07-25 17:21:22 -0700

    Better YAML strategy for glossaries

---
+++ data-raw/create_glossaries.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+
+methods <- c("tidy", "glance", "augment")
+
+recode_method <- function(df) {
+  mutate(df, method = recode(method, "1" = "tidy", "2" = "glance", "3" = "augment"))
+}
+
+load_yaml <- function(method, columns = TRUE) {
+  prefix <- if (columns) "columns" else "arguments"
+  yml_file <- paste0("data-raw/", prefix, "_", method, ".yaml")
+
+  yml <- yaml::read_yaml(yml_file)
+  # rewrite yaml alphabetically for convenience
+  yml <-  yml[order(names(yml))]
+  write_yaml(yml, yml_file)
+
+  yml %>%
+    unlist() %>%
+    tibble::enframe("column", "description")
+}
+
+column_glossary <- methods %>%
+  map_df(load_yaml, .id = "method") %>%
+  recode_method()
+
+argument_glossary <- methods %>%
+  map_df(load_yaml, columns = FALSE, .id = "method") %>%
+  recode_method()
+
+usethis::use_data(argument_glossary, column_glossary, overwrite = TRUE)
+
